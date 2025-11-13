66c5d32b6f2c95409dee7609e699ba8aa49299d3
Winter Murphy <finnlachlanmurphy@gmail.com>
2025-11-13 22:29:24 +1100

    changed request_path_to_POI method name

--- scripts/crew.gd
+++ scripts/crew.gd
@@ -2,7 +2,7 @@ class_name Crew
 extends Node2D
 ## Management class for the NPC crew members of Station Tilda.
 
-## Dictionary for storing point of interests map positions
+## Dictionary for storing point-of-interest map positions
 const POI_DICT : Dictionary = {
 	"AnalysisPC": [Vector2i(13,17)],
 	"Door": [Vector2i(4,5), Vector2i(4,17), Vector2i(8,11), Vector2i(10,5), Vector2i(10,17), Vector2i(18,8), Vector2i(18,14), Vector2i(21,11)],
@@ -16,5 +16,6 @@ const POI_DICT : Dictionary = {
 func _ready() -> void:
 	pass
 
-func request_path_to_POI(poi_name, index) -> Vector2i:
+## Method for allowing child crewmembers to access POI map positions
+func get_POI_pos(poi_name, index) -> Vector2i:
 	return POI_DICT[poi_name][index]
