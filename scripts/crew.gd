666e52c609a50f0fda57d4eb23392d26d1f8339c
Winter Murphy <finnlachlanmurphy@gmail.com>
2025-11-13 22:27:16 +1100

    filled in POI_DICT

--- scripts/crew.gd
+++ scripts/crew.gd
@@ -4,12 +4,13 @@ extends Node2D
 
 ## Dictionary for storing point of interests map positions
 const POI_DICT : Dictionary = {
-	"Door": [],
-	"ResearchPC": [],
-	"AnalysisPC": [],
-	"TildaPC": [],
+	"AnalysisPC": [Vector2i(13,17)],
+	"Door": [Vector2i(4,5), Vector2i(4,17), Vector2i(8,11), Vector2i(10,5), Vector2i(10,17), Vector2i(18,8), Vector2i(18,14), Vector2i(21,11)],
+	"ExternalAirlock": [Vector2i(23,11)],
+	"Oxygen": [Vector2i(13,5)],
 	"Reactor": [Vector2i(1,5)],
-	"Oxygen": [Vector2i(13,5)]
+	"ResearchPC": [Vector2i(17,16), Vector2i(17,18), Vector2i(17,20)],
+	"TildaPC": [Vector2i(1,17)]
 }
 
 func _ready() -> void:
